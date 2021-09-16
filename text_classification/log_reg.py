2c07eb6d2cb7277d2a05085d2f833b0680866aaf
Egor Kondrashov <e.kondr01@gmail.com>
2021-09-16 14:21:35 +0300

    feat: score doc2vec and add keras code

--- text_classification/log_reg.py
+++ text_classification/log_reg.py
@@ -26,7 +26,7 @@ def score_log_reg(csv_filename: str) -> float:
     X = df.review
     y = df.sentiment
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42)
+        X, y, test_size=0.3)
 
     y_pred: Pipeline = log_reg(X_train, y_train, X_test, y_test)
 
